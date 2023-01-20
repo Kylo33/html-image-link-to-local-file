eb6e18a42ff49cee628a795de0c17b11b4b72401
renntg <renntg@gmail.com>
2023-01-19 21:13:43 -0700

    added command-line-arguments

--- replace_img_links.py
+++ replace_img_links.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import pathlib
+import argparse
 
 def convert_to_local_images(input_html_file: str, output_html_file: str):
     with open(fr"{input_html_file}", "r") as html_file:
@@ -13,12 +14,13 @@ def convert_to_local_images(input_html_file: str, output_html_file: str):
             response = requests.get(img_src)
 
             # make the folder if it doesnt exist
-            my_path = f"images/{pathlib.Path(input_html_file).stem}/"
+            stem = pathlib.Path(input_html_file).stem
+            my_path = f"images/{stem}/"
             if not os.path.exists(my_path):
                 os.makedirs(my_path)
             
             # save the image
-            new_src = f"{my_path}{input_html_file}_{index}"
+            new_src = f"{my_path}{stem}_{index}"
             with open(new_src, "wb") as image_to_write:
                 image_to_write.write(response.content)
 
@@ -30,5 +32,9 @@ def convert_to_local_images(input_html_file: str, output_html_file: str):
         html_file.write(soup.prettify())
 
 if __name__ == "__main__":
-    for file in ["BL.html", "BR.html", "FL.html", "FR.html"]:
-        convert_to_local_images(file, file.replace(".html", "_local_images.html"))
+    parser = argparse.ArgumentParser(description="replaces html image links with local images")
+    parser.add_argument("input_file", type=str, help="enter the name of the html file you want to replace the links in. Ex: 'my_website.html'")
+    parser.add_argument("output_file", type=str, help="enter the name of the html file you want to save the output in. Ex: 'my_website_local_imgs.html'")
+    args = parser.parse_args()
+
+    convert_to_local_images(args.input_file, args.output_file)
